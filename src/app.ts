03e42f2c859daecc2048102ea02264d86f1a4fa3
Bryan Soares <bryanluccas@hotmail.com>
2025-02-24 20:38:02 -0300

    feat: add new logs

--- src/app.ts
+++ src/app.ts
@@ -26,15 +26,22 @@ import { populateDB } from "./functions/populate.js";
     concurrency,
   };
 
-  console.info(chalk.blueBright.bold("Starting to populate the database with the following options:"));
-  console.log(chalk.bold.yellow("DB URI: "), chalk.yellowBright(dbUri));
-  console.log(chalk.bold.yellow("DB Name: "), chalk.yellowBright(dbName));
-  console.log(chalk.bold.yellow("Batch Size: "), chalk.yellowBright(batchSize));
-  console.log(chalk.bold.yellow("Collection Size: "), chalk.yellowBright(collectionSize));
-  console.log(chalk.bold.yellow("Concurrency: "), chalk.yellowBright(concurrency));
-  console.log(chalk.bold.yellow("Simple Collections: "), chalk.yellowBright(simpleCollections));
-  console.log(chalk.bold.yellow("Complex Collections: "), chalk.yellowBright(complexCollections));
+  console.info(chalk.gray.bold("\nStarting to populate the database with the following options"));
+  console.info(chalk.bold.dim("| DB URI: "), chalk.dim(dbUri));
+  console.info(chalk.bold.dim("| DB Name: "), chalk.dim(dbName));
+  console.info(chalk.bold.dim("| Batch Size: "), chalk.dim(batchSize));
+  console.info(chalk.bold.dim("| Collection Size: "), chalk.dim(collectionSize));
+  console.info(chalk.bold.dim("| Concurrency: "), chalk.dim(concurrency));
+  console.info(chalk.bold.dim("| Simple Collections: "), chalk.dim(simpleCollections));
+  console.info(chalk.bold.dim("| Complex Collections: "), chalk.dim(complexCollections), '\n');
+  console.info(chalk.whiteBright("Population in progress..."))
 
+  const start = Date.now();
   await populateDB(options);
+  const end = Date.now();
   client.close();
+
+  console.info(chalk.bold.greenBright("[ Database ] Connection to the database closed."));
+  console.info(chalk.bold.gray("\nDatabase population time: "), chalk.bold.dim(`${(end - start) / 1000}s`));
+
 })();
