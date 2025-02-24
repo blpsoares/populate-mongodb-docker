77e595a2701f6a5a5f41f88d94397c5ca73e3e7e
Bryan Soares <bryanluccas@hotmail.com>
2025-02-24 20:38:17 -0300

    style: change logs color

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -2,11 +2,11 @@ import chalk from 'chalk';
 import { MongoClient } from 'mongodb';
 
 export const conn = async (URI: string) => {
-  console.log(chalk.yellow.bold("[ Connecting to database ]", URI));
+  console.log(chalk.yellow.bold("[ Database ] Connecting to -> ", URI));
   const client = new MongoClient(URI);
   try {
     await client.connect();
-    console.log(chalk.green.bold("[ Database connected ]"));
+    console.log(chalk.green.bold("[ Database ] Conected with successfully!"));
     return client;
   } catch (e) {
     throw `Error to connect to database: ${e}`;
